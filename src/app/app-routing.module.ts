e9b4e6e2dc2771ecde4d695d8e5ce1c6d2a096a9
Ashwath Bharadwaj <ashwath.bharadwaj@outlook.com>
2021-05-20 03:55:35 +0530

    Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlankComponent } from './layouts/blank/blank.component';
+import { FullComponent } from './layouts/full/full.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BlankComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./modules/features/landing/landing.module').then(
+            (m) => m.LandingModule
+          )
+      },
+      {
+        path: 'blog',
+        loadChildren: () =>
+          import('./modules/features/blog/blog.module').then(
+            (m) => m.BlogModule
+          )
+      }
+    ]
+  },
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('./modules/features/home/home.module').then(
+            (m) => m.HomeModule
+          )
+      }
+    ]
+  },
+
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
