8ef0d8722d3907502eeadecea4ccf66dee0c3d48
Ashwath Bharadwaj <ashwath.bharadwaj@outlook.com>
2021-05-23 22:06:53 +0530

    Dynamic categories

--- src/app/modules/features/home/components/categories/categories.component.ts
+++ src/app/modules/features/home/components/categories/categories.component.ts
@@ -13,8 +13,45 @@ export class CategoriesComponent implements OnInit, OnDestroy {
 
   navigationSubscription;
   currentRoute: any = '';
-
   mainLoader: boolean = false;
+  categoriesList: any = [
+    {
+      name: 'Men',
+      imageUrl: 'https://images.unsplash.com/photo-1531471689044-dd3ca86632e1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2250&q=80',
+      routeUrl: '',
+      packsCount: 4
+    },
+    {
+      name: 'Women',
+      imageUrl: 'https://images.unsplash.com/photo-1510877073473-6d4545e9c2af?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80',
+      routeUrl: '',
+      packsCount: 4
+    },
+    {
+      name: 'Essentials',
+      imageUrl: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1234&q=80',
+      routeUrl: '',
+      packsCount: 4
+    },
+    {
+      name: 'Exotic',
+      imageUrl: 'https://images.unsplash.com/photo-1526827826797-7b05204a22ef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80',
+      routeUrl: '',
+      packsCount: 4
+    },
+    {
+      name: 'Fashion',
+      imageUrl: 'https://images.unsplash.com/photo-1519961655809-34fa156820ff?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1234&q=80',
+      routeUrl: '',
+      packsCount: 4
+    },
+    {
+      name: 'Kids',
+      imageUrl: 'https://images.unsplash.com/photo-1556910585-09baa3a3998e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1301&q=80',
+      routeUrl: '',
+      packsCount: 4
+    },
+  ];
 
   constructor(
     private router: Router,
@@ -36,8 +73,13 @@ export class CategoriesComponent implements OnInit, OnDestroy {
     // this.mainLoader = false;
   }
 
-  goToSubCategory() {
+  goToSubCategoryTest() {
     const routeUrl = `/home/${this.currentRoute}/all`;
+    this.router.navigateByUrl(routeUrl);
+  }
+
+  goToSubCategory(categoryObj: any) {
+    const routeUrl = `/home/${this.currentRoute}/${categoryObj.name}`;
     this.router.navigateByUrl(routeUrl);
   }
 
