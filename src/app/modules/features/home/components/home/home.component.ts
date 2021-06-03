ad2656ebf0572e8776771463f00d73b23455d322
Ashwath Bharadwaj <ashwath.bharadwaj@outlook.com>
2021-06-04 01:38:52 +0530

    Home

--- src/app/modules/features/home/components/home/home.component.ts
+++ src/app/modules/features/home/components/home/home.component.ts
@@ -10,6 +10,43 @@ export class HomeComponent implements OnInit {
 
   packType: any = 'store';
 
+  cards: any = [{
+    name: 'Store',
+    content: 'Products delivered every month',
+    imageUrl: 'imageUrl',
+    routerUrl: '/home/store'
+  },
+  {
+    name: 'Services',
+    content: 'Curated Services right to your home',
+    imageUrl: 'imageUrl',
+    routerUrl: '/home/services'
+  },
+  {
+    name: 'Finances',
+    content: 'Never miss your bill payments',
+    imageUrl: 'imageUrl',
+    routerUrl: '/home/finances'
+  },
+  {
+    name: 'Investments',
+    content: 'Investments on steroids',
+    imageUrl: 'imageUrl',
+    routerUrl: '/home/investments'
+  },
+  {
+    name: 'Crowdfunding',
+    content: 'Invest once, earn monthly returns',
+    imageUrl: 'imageUrl',
+    routerUrl: '/home/crowdfunding'
+  },
+  {
+    name: 'Dashboard',
+    content: 'Track your monthly spend',
+    imageUrl: 'imageUrl',
+    routerUrl: 'dashboard'
+  }];
+
   constructor(
     private router: Router
   ) { }
@@ -20,7 +57,6 @@ export class HomeComponent implements OnInit {
 
   initLoad() {
     let document: any;
-    
   }
 
   goToPacks() {
